19606a1bc222e1f8121badc1e68012ca7962543d
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-09 22:54:16 +0330

    add AutherRepository to project and implement save method

---
+++ src/main/java/org/example/repository/AuthorRepository.java
@@ -0,0 +1,31 @@
+package org.example.repository;
+
+import org.example.entity.Auther;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AuthorRepository {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    public AuthorRepository() throws Exception{
+        Class.forName("org.postgresql.Driver");
+        connection= DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","myjava123");
+    }
+
+    public void save(Auther auther) throws Exception
+
+    {
+        preparedStatement=connection.prepareStatement("insert into auther values (?,?,?,?);");
+        preparedStatement.setInt(1,auther.getId());
+        preparedStatement.setString(2, auther.getName());
+        preparedStatement.setString(3, auther.getFamily());
+        preparedStatement.setInt(4,auther.getAge());
+        preparedStatement.close();
+        System.out.println("Auther Saved Successfully");
+    }
+}
