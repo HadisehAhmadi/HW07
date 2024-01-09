190c76db8f50fc72504dcb518a1e3d755f5b5227
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-09 23:10:11 +0330

    test load method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,14 @@
 package org.example;
+
+import org.example.entity.Auther;
+import org.example.repository.AuthorRepository;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        AuthorRepository authorRepository = new AuthorRepository();
+        //Auther auther = new Auther(4,"Jules","Verne",77);
+        //authorRepository.save(auther);
+        Auther auther = authorRepository.load(4);
+
+    }
 }
