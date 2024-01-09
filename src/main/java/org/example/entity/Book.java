ace83b8d155161724c43bb09238bbd369a43eb07
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-09 22:33:43 +0330

    add Book.java

---
+++ src/main/java/org/example/entity/Book.java
@@ -0,0 +1,46 @@
+package org.example.entity;
+
+public class Book {
+    private int id;
+    private String title;
+    private int year;
+
+    public Book(int id, String title, int year) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
