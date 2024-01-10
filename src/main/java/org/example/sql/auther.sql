7875e866696d704c6fc6e8589d0a7bf469dd083a
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-10 21:59:20 +0330

    update db tables

--- src/main/java/org/example/sql/auther.sql
+++ src/main/java/org/example/sql/auther.sql
@@ -1,30 +1,47 @@
-create table if not exists auther (id serial primary key ,name varchar(20), family varchar(20),age int);
-create table if not exists book(id serial primary key ,title varchar(20),year int);
-create table if not exists authers_books(id serial primary key, autherID int references auther(id),bookID int references book(id));
+create table if not exists auther
+(
+    id     serial unique primary key,
+    name   varchar(20),
+    family varchar(20),
+    age    int
+);
+create table if not exists book
+(
+    id       serial unique primary key,
+    title    varchar(20),
+    year     int,
+    autherID int references auther (id)
+);
 
-insert into auther values (1,'Charles','Dickens',58);
-insert into auther values (2,'Jane','Austen',42);
-insert into auther values (3,'Leo','Tolstoy',82);
+
+insert into auther
+values (DEFAULT,'Charles', 'Dickens', 58);
+insert into auther
+values (DEFAULT,'Jane', 'Austen', 42);
+insert into auther
+values (DEFAULT,'Leo', 'Tolstoy', 82);
 
 
-insert into book values (1,'Oliver Twis',1838);
-insert into book values (2,'A Tale of Two Cities',1859);
-insert into book values (3,'Pride and Prejudice',1813);
-insert into book values (4,'Anna Karenina',1878);
-insert into book values (5,'War and Peace',1865);
+insert into book
+values (DEFAULT, 'Oliver Twis', 1838,3);
+insert into book
+values (DEFAULT, 'A Tale of Two Cities', 1859,3);
+insert into book
+values (DEFAULT, 'Pride and Prejudice', 1813,1);
+insert into book
+values (DEFAULT, 'Anna Karenina', 1878,2);
+insert into book
+values (DEFAULT, 'War and Peace', 1865,2);
 
-insert into authers_books values (1,1,1);
-insert into authers_books values (2,1,2);
-insert into authers_books values (3,2,3);
-insert into authers_books values (4,3,4);
-insert into authers_books values (5,3,5);
+select *
+from auther;
+select *
+from book;
 
-select * from auther;
-select * from book;
-select * from authers_books;
 
-select name,family,title from authers_books
-                inner join auther a on autherID = a.id
-                inner join book b on bookID = b.id
-where name='Leo';
+select name, family, title
+from auther
+         inner join book b on auther.id = b.autherID
+where name = 'Leo';
+
 
